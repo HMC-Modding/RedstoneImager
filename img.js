ef4a2534372eba7e61336d9757ff5d11dc551256
HMC-Modding <152757396+HMC-Modding@users.noreply.github.com>
2024-03-02 21:00:33 -0500

    Update img.js

--- img.js
+++ img.js
@@ -1,4 +1,4 @@
-import mergeImages from 'merge-images';
+//import mergeImages from 'merge-images';
  
 mergeImages(['/un.png', '/un.png', '/mouth.png'])
   .then(b64 => document.querySelector('img').src = b64);
