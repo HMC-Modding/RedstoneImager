6359c7b985e822949d15d593a3710ff9a26d3267
HMC-Modding <152757396+HMC-Modding@users.noreply.github.com>
2024-03-04 08:36:03 -0500

    Update img.js

--- img.js
+++ img.js
@@ -1,4 +1,8 @@
 import mergeImages from 'merge-images';
  
-mergeImages(['/RedstoneImager/un.png', '/RedstoneImager/un.png', '/RedstoneImager/un.png'])
+mergeImages([
+  { src: 'https://hmc-modding.github.io/RedstoneImager/un.png', x: 0, y: 0 },
+  { src: 'https://hmc-modding.github.io/RedstoneImager/un.png', x: 64, y: 0 },
+  { src: 'https://hmc-modding.github.io/RedstoneImager/un.png', x: 128, y: 0 }
+])
   .then(b64 => document.querySelector('img').src = b64);
