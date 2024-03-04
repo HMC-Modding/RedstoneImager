ae53ae59c6f37b6a7602cd5f3e90b63a04a492c1
HMC-Modding <152757396+HMC-Modding@users.noreply.github.com>
2024-03-04 14:26:25 -0500

    Update img.js

--- img.js
+++ img.js
@@ -1,8 +1,26 @@
 import mergeImages from 'merge-images';
- 
-mergeImages([
-  { src: 'https://hmc-modding.github.io/RedstoneImager/un.png', x: 0, y: 0 },
-  { src: 'https://hmc-modding.github.io/RedstoneImager/un.png', x: 64, y: 0 },
-  { src: 'https://hmc-modding.github.io/RedstoneImager/un.png', x: 128, y: 0 }
-])
-  .then(b64 => document.querySelector('img').src = b64);
+
+var img = document.querySelector('img');
+
+var button = document.getElementById("pressButton");
+
+function def() {
+
+  var width = document.getElementById("widthInput").value;
+  var height = document.getElementById("heightInput").value;
+  
+  mergeImages([
+    { src: 'https://hmc-modding.github.io/RedstoneImager/un.png', x: 0, y: 0 },
+    { src: 'https://hmc-modding.github.io/RedstoneImager/un.png', x: 160, y: 0 },
+    { src: 'https://hmc-modding.github.io/RedstoneImager/un.png', x: 0, y: 0 }
+  ], {
+    width: 160*parseInt(width),
+    height: 160*parseInt(height)
+  })
+    .then(b64 => img.src = b64);
+
+  img.setAttribute("width", (parseInt(width)*16).toString());
+  img.setAttribute("height", (parseInt(height)*16).toString());
+}
+
+button.addEventListener("click", def)
